963abf48b961fceb54d641ce140ced70d3f8ff3c
AIE\s171365 <theo.inc.au@gmail.com>
2017-07-24 11:20:33 +0930

    Unit Testing Stuff

---
+++ Unit Testing/Unit Test/Unit Test/Unit Test.cpp
@@ -0,0 +1,10 @@
+// Unit Test.cpp : Defines the entry point for the console application.
+//
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+int main()
+{
+    return 0;
+}
+
